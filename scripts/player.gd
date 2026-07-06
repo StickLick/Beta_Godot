cfdf57cb13d23e087ea6ec8fb3953aa621cca591
StickLick <o.fedotov.dodo@gmail.com>
2026-07-06 21:01:00 +0300

    1
    1

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends CharacterBody2D
+
+@export var max_speed: float = 250.0
+@export var acceleration: float = 1800.0
+@export var friction: float = 1500.0
+
+func _physics_process(delta: float) -> void:
+	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+
+	move_and_slide()
