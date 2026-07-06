2f542839d65afd1c8c4e995037bd4fd1bb55e7a4
Optimist98 <61421755+Optimist98@users.noreply.github.com>
2026-07-06 21:10:44 +0300

    2

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,11 +5,11 @@ extends CharacterBody2D
 @export var friction: float = 1500.0
 
 func _physics_process(delta: float) -> void:
-	var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+    var input_vector: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
-	if input_vector != Vector2.ZERO:
-		velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
-	else:
-		velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
+    if input_vector != Vector2.ZERO:
+        velocity = velocity.move_toward(input_vector * max_speed, acceleration * delta)
+    else:
+        velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 
-	move_and_slide()
+    move_and_slide()
